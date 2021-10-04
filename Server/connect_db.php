1df7fb1c0bf1b542d4bd19bf0d9c0dd310cb3079
smnam <sumannam@gmail.com>
2021-10-04 19:14:07 +0900

    hidden-tear-release 폴더 생성

--- Server/connect_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-	//change you connection options
-	$con=mysqli_connect("localhost","root","techfin", "hidden_tear");	 
-        
-    if (!$con){
-     die ("Colud not connect to server: ".mysql_error());
-    }
-?>
